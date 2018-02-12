34114cf5990babecb92f306d880286d6f8d60491
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-02-12 21:52:33 +0000

    Experiments

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for repa-experiments
+
+## Unreleased changes
---
+++ app/Experiments.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE BangPatterns #-}
+module Experiments where
+
+import Data.Array.Repa as Repa
+import Data.Functor.Identity
+
+type Weight = Int
+type Graph r = Array r DIM2 Weight
+
+shortestPath :: Graph U -> Graph U
+shortestPath g0 = runIdentity $ go g0 0
+  where Z :. _ :. n = extent g0 -- this is basically giving dimensions
+        go !g !k
+          | k == n = return g
+          | otherwise = do
+              g' <- computeP (fromFunction (Z:.n:.n) sp)
+              go g' (k + 1)
+          where
+            sp (Z:.i:.j) = min (g ! (Z:.i:.j)) (g ! (Z:.i:.k) + g ! (Z:.k:.j))
